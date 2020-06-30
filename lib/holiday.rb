c7bc6726486598f7f994923d5c232e2588bbcb1e
Lief Friedrichs <lief.friedrichs@gmail.com>
2020-06-30 19:37:34 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -60,7 +60,7 @@ def all_supplies_in_holidays(holiday_hash)
   #   Fourth Of July: Fireworks, BBQ
   # etc.
   holiday_hash.each {|season, holidays| 
-    puts season.to_s.capitalize + ':'
+    puts season.to_s.capitalize + ': '
     holidays.each {|holiday, supplies| 
       puts holiday.to_s.capitalize + ":" + supplies.join(', ') } 
     }
