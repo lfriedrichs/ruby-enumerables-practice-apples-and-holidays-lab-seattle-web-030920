203a38d1faaed04125e7fae9c45f0b8cfd5554e1
Lief Friedrichs <lief.friedrichs@gmail.com>
2020-06-30 19:38:50 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -62,7 +62,7 @@ def all_supplies_in_holidays(holiday_hash)
   holiday_hash.each {|season, holidays| 
     puts season.to_s.capitalize + ':'
     holidays.each {|holiday, supplies| 
-      puts "  " + holiday.to_s.capitalize + ": " + supplies.join(', ') } 
+      puts "  " + holiday.to_s.split('_').capitalize.join(' ') + ": " + supplies.join(', ') } 
     }
 
 end
